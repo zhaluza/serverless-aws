4942767e111f8754ff70e7cc1658fe432e0acf0b
Zac H <zac.haluza@gmail.com>
2021-08-27 07:30:42 -0400

    set webpack mode to 'none'

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+/**
+ * Use mode: 'production' to minify code.
+ * Using 'none' gives you an advantage while developing,
+ * since the logs will tell you where the code failed
+ */
+
 module.exports = {
   target: "node",
-  mode: "production",
+  mode: "none",
 };
